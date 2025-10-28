8f7a6b36c137fa1af7b2eba1fa0752bf4e303c53
Nikhil <nikhilnov28@gmail.com>
2025-10-28 16:13:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
+import ImageGenerator from './components/ImageGenerator';
+import ChatComponent from './components/ChatComponent';
 import './App.css';
+import { tab } from '@testing-library/user-event/dist/tab';
 
 function App() {
+  const[activeTab, setActivetab]=useState('image-generator');
+  const handleTabChange=(tab)=>{
+    setActivetab(tab);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className={activeTab === 'image-generator' ? 'active' : ''}
+       onClick={() => handleTabChange('image-generator')}>
+        Image Generator
+        </button>
+      <button  className={activeTab === 'chat' ? 'active' : ''}
+      onClick={() => handleTabChange('chat')}>
+        Ask AI
+        </button>
+        <div>
+          {activeTab === 'image-generator' && <ImageGenerator/>}
+          {activeTab === 'chat' && <ChatComponent/>}
+        </div>
     </div>
   );
 }
