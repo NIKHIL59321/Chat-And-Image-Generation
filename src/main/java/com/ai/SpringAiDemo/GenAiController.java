f7f9057d5f0f7ad635bfd0b77fba09aadfe3dcef
Nikhil <nikhilnov28@gmail.com>
2025-10-28 16:03:43 +0530

    commit

---
+++ src/main/java/com/ai/SpringAiDemo/GenAiController.java
@@ -0,0 +1,53 @@
+package com.ai.SpringAiDemo;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GenAiController {
+    private final ChatService chatService;
+    private final ImageService imageService;
+
+    public GenAiController(ChatService chatService, ImageService imageService) {
+        this.chatService = chatService;
+        this.imageService = imageService;
+    }
+    @GetMapping("ask-ai")
+    public String getResponse(@RequestParam String prompt){
+        return chatService.getResponse(prompt);
+    }
+
+    @GetMapping("ask-ai-options")
+    public String getResponseOptions(@RequestParam String prompt){
+        return chatService.getResponseOptions(prompt);
+    }
+
+    /*@GetMapping("generate-image")
+    public void generateImage(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse= imageService.generateImage(prompt);
+        String imageUrl=imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
+    }*/
+
+    @GetMapping("generate-image")
+    public List<String> generateImages(HttpServletResponse response,
+                                       @RequestParam String prompt,
+                                       @RequestParam(defaultValue = "1") int n,
+                                       @RequestParam(defaultValue = "1024") int width,
+                                       @RequestParam(defaultValue = "1024") int height) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt, n, width, height);
+
+        // Streams to get urls from ImageResponse
+        List<String> imageUrls = imageResponse.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
+
+        return imageUrls;
+    }
+
+}
